6038d81732956a1a13d60b3c30127cc795c06034
Corey Clippinger <coreyclip@gmail.com>
2019-09-22 14:37:01 -0700

    by index json generation

--- data/etl.py
+++ data/etl.py
@@ -2,35 +2,28 @@ import os
 from pathlib import Path
 import json
 import pandas as pd
-
 data = {}
 for file in os.listdir('raw_data'):
     fp = Path(f"raw_data/{file}")
-    dfs = pd.read_excel(fp) 
-    print(fp.stem)
-    if isinstance(dfs, pd.DataFrame):
-        df = dfs.fillna(0)
-        df.index = df.iloc[:, 0]
-        dict_ = df.to_json(orient='records')
-        with open(f'json/{fp.stem}.json', 'w') as outfile:
-            json.dump(dict_, outfile)
-    else:
-        for name, sheet in dfs.items():
-            print(name)
-            try:
-                print(sheet.info())
-                print('====' * 20)
-                print(sheet.head(10))
-            except:
-                print(sheet)
-
-            cont = input('save? y/n >>>>')
-            if cont == 'n':
-                pass
-            else:
-                name = input('Name for json file >>>> ')
-                index_col = input('Index column >>> ')
-                dict_ = sheet.set_index(index_col).to_json(orient='records')
-                with open(f'json/{name}.json', 'w') as outfile:
-                    json.dump(dict_, outfile)
+    df = pd.read_excel(fp) 
+    indicator = fp.stem 
+    first_col_name = df[df.columns[0]].name
+    try:
+        int(first_col_name)
+        if df.index.name == None:
+            df.index.name = 'Country'
+    except:
+        df.set_index(df.columns[0], inplace=True)
+        df.index.name = 'Country'
+    print('===' * 20)
+    print(indicator)
+    print('===' * 20)
+    print(df.head().to_dict(orient='index'))
+    data[indicator] = df.to_dict(orient='index')
+    
+with open(f'json/master_data.json', 'w') as outfile:
+        json.dump(data, outfile)
+        
+    
+    
 
